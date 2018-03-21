91ff199659e4f879dcca364d505be190cd652622
JulianBruxelle <36154424+JulianBruxelle@users.noreply.github.com>
2018-03-21 16:38:45 +0100

    Creation primitives fonctions / type.

--- functions.h
+++ functions.h
@@ -1,1 +1,17 @@
+#ifndef FG_FrD
+#define FG_FrD
 
+typedef strut noeud {
+    unsigned char lettre;
+    strut noeud *filsg, *frered;
+} Noeud, *Arbre;
+
+#endif FG_FrD
+
+void afficheLexique (Arbre a);
+
+void sauvegardeMots (Arbre a, FILE *out);
+
+int recherche (Arbre a, char *mot);
+
+void sauvegardeArbre (Arbre a ,FILE *out);
